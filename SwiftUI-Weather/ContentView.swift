1d190dfc5d8c6b16457a7ccdf7d9e5edcb7e890e
“Diana <dduong013@gmail.com>
2023-11-18 12:23:38 -0600

    initial commit

---
+++ SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by Diana Duong on 11/18/23.
+//  Copyright © 2023 Diana Duong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
