daf3ea4773dcfe176426964860a823da118a1e30
“Diana <dduong013@gmail.com>
2023-11-20 13:24:51 -0600

    refactor code

--- SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
@@ -11,40 +11,35 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, Color("lightBlue")]),
-                           startPoint: .top,
-                           endPoint: .bottomTrailing)
-                .edgesIgnoringSafeArea(.all)
+            
+            BackgroundView(topColor: .blue, bottomColor: Color("lightBlue"))
+            
             VStack {
-              Text("Cupertino, CA")
-                    .font(.system(size: 32, weight: .medium, design: .default))
-                    .foregroundColor(.white)
-                .padding(.top, 40)
+                CityTextView(cityName: "Cupertino, CA")
                 
-                VStack(spacing: 8) {
-                    Image("icon-sun-large-cloud")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 180, height: 180)
-                    
-                    Text("76°")
-                        .font(.system(size: 70, weight:
-                            .medium))
-                        .foregroundColor(.white)
-                }
-                .padding(.bottom, 40)
+                MainWeatherView(mainWeatherImage: "icon-sun-large-cloud",
+                                mainTemperature: 76)
 
                 HStack(spacing: 20){
-                    WeatherWeekView(dayOfWeek: "SUN", weatherImage: "icon-sun-large-cloud", temperature: 60)
-                  
-                    WeatherWeekView(dayOfWeek: "MON", weatherImage: "icon-cloud", temperature: 50)
-                  
-                     WeatherWeekView(dayOfWeek: "TUES", weatherImage: "icon-sun-rain-cloud", temperature: 60)
-                    
-                     WeatherWeekView(dayOfWeek: "WED", weatherImage: "icon-sun-rain-cloud", temperature: 60)
-                                           
-                     WeatherWeekView(dayOfWeek: "THURS", weatherImage: "icon-sun", temperature: 80)
+                    WeatherWeekView(dayOfWeek: "SUN",
+                                    weatherImage: "icon-sun-large-cloud",
+                                    temperature: 60)
+
+                    WeatherWeekView(dayOfWeek: "MON",
+                                    weatherImage: "icon-cloud",
+                                    temperature: 50)
+
+                    WeatherWeekView(dayOfWeek: "TUES",
+                                    weatherImage: "icon-sun-rain-cloud",
+                                    temperature: 60)
+
+                    WeatherWeekView(dayOfWeek: "WED",
+                                    weatherImage: "icon-sun-rain-cloud",
+                                    temperature: 60)
+                       
+                    WeatherWeekView(dayOfWeek: "THURS",
+                                    weatherImage: "icon-sun",
+                                    temperature: 80)
                 }
                 
                 Spacer()
@@ -96,5 +91,53 @@ struct WeatherWeekView: View {
                     .medium))
                 .foregroundColor(.white)
         }
+    }
+}
+
+struct BackgroundView: View {
+    
+    var topColor: Color
+    var bottomColor: Color
+
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]),
+                       startPoint: .top,
+                       endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CityTextView: View {
+    
+    var cityName: String
+    
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding(.top, 40)
+    }
+}
+
+struct MainWeatherView: View {
+    
+    var mainWeatherImage: String
+    var mainTemperature: Int
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Image(mainWeatherImage)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            
+            Text("\(mainTemperature)°")
+                .font(.system(size: 70, weight:
+                    .medium))
+                .foregroundColor(.white)
+        }
+        .padding(.bottom, 40)
     }
 }
