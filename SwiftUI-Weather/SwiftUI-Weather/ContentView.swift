c6132f65ce6a2e4523eb51167abea53ab35f1348
“Diana <dduong013@gmail.com>
2023-12-02 15:50:36 -0600

    add state & binding

--- SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
@@ -1,77 +1,90 @@
-    //
-    //  ContentView.swift
-    //  SwiftUI-Weather
-    //
-    //  Created by Diana Duong on 11/18/23.
-    //  Copyright © 2023 Diana Duong. All rights reserved.
-    //
+//
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by Diana Duong on 11/18/23.
+//  Copyright © 2023 Diana Duong. All rights reserved.
+//
 
-    import SwiftUI
+import SwiftUI
 
-    struct ContentView: View {
-        
-        @State private var isNight = false
-        
-        var body: some View {
-            ZStack {
+struct ContentView: View {
+//structs are value types - they get destroyed and recreated
+    
+    //@State property allows a struct to hold a state
+    @State private var isNight = false
+    
+    var body: some View {
+        ZStack {
+            
+            BackgroundView(isNight: $isNight)
+            
+            VStack {
+                CityTextView(cityName: "Cupertino, CA")
                 
-                //if topColor is night = change the top gradient to black else change to blue
-                //if bottom color is night = change the bottom gradient to gray else change to light blue
-                BackgroundView(topColor: isNight ? .black : .blue,
-                               bottomColor: isNight ? .gray : Color("lightBlue"))
-                
-                
-                VStack {
-                    CityTextView(cityName: "Cupertino, CA")
-                    
-                    MainWeatherView(mainWeatherImage: isNight ? "icon-moon-cloud" : "icon-sun-large-cloud",
-                                    mainTemperature: 76)
+                MainWeatherView(mainWeatherImage: isNight ? "icon-moon-cloud" : "icon-sun-large-cloud",
+                                mainTemperature: 76)
 
-                    HStack(spacing: 20){
-                        WeatherWeekView(dayOfWeek: "SUN",
-                                        weatherImage: isNight ? "icon-moon-cloud" : "icon-cloud",
-                                        temperature: 60)
+                HStack(spacing: 20){
+                    WeatherWeekView(dayOfWeek: "SUN",
+                                    weatherImage: isNight ? "icon-moon-cloud" : "icon-cloud",
+                                    temperature: 60)
 
-                        WeatherWeekView(dayOfWeek: "MON",
-                                        weatherImage: isNight ? "icon-cloud-rain" : "icon-cloud",
-                                        temperature: 50)
+                    WeatherWeekView(dayOfWeek: "MON",
+                                    weatherImage: isNight ? "icon-cloud-rain" : "icon-cloud",
+                                    temperature: 50)
 
-                        WeatherWeekView(dayOfWeek: "TUES",
-                                        weatherImage: isNight ? "icon-moon-stars" : "icon-sun-rain-cloud",
-                                        temperature: 60)
+                    WeatherWeekView(dayOfWeek: "TUES",
+                                    weatherImage: isNight ? "icon-moon-stars" : "icon-sun-rain-cloud",
+                                    temperature: 60)
 
-                        WeatherWeekView(dayOfWeek: "WED",
-                                        weatherImage: "icon-cloud-rain",
-                                        temperature: 60)
-                           
-                        WeatherWeekView(dayOfWeek: "THURS",
-                                        weatherImage: isNight ? "icon-moon" : "icon-sun",
-                                        temperature: 80)
-                    }
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                        self.isNight.toggle()
-                    }) {
-                        WeatherButton(title: "Change Day Time",
-                                      textColor: isNight ? .white : .blue,
-                                      backgroundColor: isNight ? .black : .white)
-                    }
-                    
-                    Spacer()
+                    WeatherWeekView(dayOfWeek: "WED",
+                                    weatherImage: "icon-cloud-rain",
+                                    temperature: 60)
+                       
+                    WeatherWeekView(dayOfWeek: "THURS",
+                                    weatherImage: isNight ? "icon-moon" : "icon-sun",
+                                    temperature: 80)
                 }
+                
+                Spacer()
+                
+                Button(action: {
+                    self.isNight.toggle()
+                }) {
+                    WeatherButton(title: "Change Day Time",
+                                  textColor: isNight ? .white : .blue,
+                                  backgroundColor: isNight ? .black : .white)
+                }
+                
+                Spacer()
             }
         }
     }
+}
 
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
+}
 
-
+struct BackgroundView: View {
+    
+    @Binding var isNight: Bool
+    
+    var body: some View {
+        
+        //if topColor is night = change the top gradient to black else change to blue
+        //if bottom color is night = change the bottom gradient to gray else change to light blue
+        LinearGradient(gradient: Gradient(colors: [ isNight ? .black : .blue,
+                                                    isNight ? .gray : Color("lightBlue")]),
+                       startPoint: .top,
+                       endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+    
 struct WeatherWeekView: View {
     
     var dayOfWeek: String
@@ -95,20 +108,6 @@ struct WeatherWeekView: View {
                     .medium))
                 .foregroundColor(.white)
         }
-    }
-}
-
-struct BackgroundView: View {
-    
-    var topColor: Color
-    var bottomColor: Color
-
-    
-    var body: some View {
-        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]),
-                       startPoint: .top,
-                       endPoint: .bottomTrailing)
-            .edgesIgnoringSafeArea(.all)
     }
 }
 
